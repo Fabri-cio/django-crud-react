d9664815df8c8773cdef5265f48aa4c26cfc176f
wil <wil.fabri777@gmail.com>
2024-09-03 22:54:59 -0400

    crear tareas

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,6 +1,38 @@
+import { useForm } from "react-hook-form";
+import { createTask } from "../api/tasks.api";
+import { useNavigate } from "react-router-dom";
+
 export function TaskFormPage() {
-    return (
-      <div>TaskFormPage</div>
-    )
-  }
-  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  const onSubmit = handleSubmit(async (data) => {
+    await createTask(data);
+    // console.log(res);
+    navigate("/tasks");
+  });
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="title"
+        {...register("title", { required: true })}
+      />
+      {errors.title && <span>title is required</span>}
+
+      <textarea
+        rows={3}
+        placeholder="Description"
+        {...register("description", { required: true })}
+      ></textarea>
+      {errors.description && <span>title is required</span>}
+
+      <button>Save</button>
+    </form>
+  );
+}
